83e223f8d98c87b0f7e0e57bdc14d5a744f9892f
Mark <markmwago@gmail.com>
2019-02-15 12:53:07 +0300

    add sides on js.

--- triangle.js
+++ triangle.js
@@ -1,3 +1,6 @@
 function display(){
-  
+var length1 = parseInt(document.getElementsByClassName('length1').value)
+var length2 = parseInt(document.getElementsByClassName('length2').value)
+var length3 = parseInt(document.getElementsByClassName('length3').value)
+if 
 }
