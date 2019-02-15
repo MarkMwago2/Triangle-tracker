09963acbadc3d6aa358739f91123d46473c75ac1
Mark <markmwago@gmail.com>
2019-02-15 15:03:10 +0300

    add changes to triangle.js.

--- triangle.js
+++ triangle.js
@@ -1,6 +1,14 @@
-function display(){
-var length1 = parseInt(document.getElementsByClassName('length1').value)
-var length2 = parseInt(document.getElementsByClassName('length2').value)
-var length3 = parseInt(document.getElementsByClassName('length3').value)
-if 
+function display() {
+var length1 = parseInt(document.getElementById('length1').value);
+var length2 = parseInt(document.getElementById('length2').value);
+var length3 = parseInt(document.getElementById('length3').value);
+if (length1 + length2 <= length3 || length2 + length3 <= length1 || length1 + length3 <= length2){
+   alert("not a TRIANGLE");
+}else if (length1===length2 && length2===length3 && length3===length1) {
+  alert("this is an equilateral triangle");
+}else if (length1===length2 || length2===length3 || length3===length1){
+  alert("this is an isosceles");
+}else{
+   alert("this is a scalene traingle");
+ }
 }
